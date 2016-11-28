460b71fdca469e27f498265a3d2cefdd34d9663c
XCode <xcode@sala6pc06.cic>
2016-11-28 19:13:18 +0100

    Initial Commit

---
+++ reproductorMp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reproductorMp3
+//
+//  Created by XCode on 28/11/16.
+//  Copyright © 2016 XCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
