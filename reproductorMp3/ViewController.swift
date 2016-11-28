550f2407036d69fac1586a9bd7424d39cf3229bd
XCode <xcode@sala6pc06.cic>
2016-11-28 20:08:18 +0100

    funcion de reproduccion

--- reproductorMp3/ViewController.swift
+++ reproductorMp3/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        playSound(soundname: "")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func playSound (soundname: String) {
+        let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        print(documentsPath.path)
+        // Carga la lista de archivos del directorio documentos
+        let fm = FileManager.default
+        let allfiles = try? fm.contentsOfDirectory(atPath: documentsPath.path)
+        print(allfiles)
+        
+        let soundname = allfiles![0] // reproduce el primero
+        let soundpathURL = documentsPath.appendingPathComponent(soundname)
+        player = try! AVAudioPlayer(contentsOf: soundpathURL)
+        player.prepareToPlay()
+        player.play()
+    }
+    
 }
 
